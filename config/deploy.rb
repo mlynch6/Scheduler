3f4ffcd9c4c259f599aa948fa60cce3b837bbaad
Melody Stephen-Hassard <mlynch6@hotmail.com>
2013-12-28 17:22:01 -0500

    Update capistrano deployment
    nginx & unicorn configuration
    nginx & unicorn start at server boot
    fixed bugs with stripe & rails startup

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,23 +7,26 @@ set :repo_url, 'https://mlynch6:g3ZGA2BRevtnU@github.com/mlynch6/Scheduler.git'
 set :branch, 'master'
 
 set :deploy_to, '/var/www/danceware'
-set :keep_releases, 20
-
-set :format, :pretty
-set :log_level, :debug
-# set :pty, true
-
-set :linked_files, %w{config/database.yml config/unicorn.rb}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 10
 
 set :rbenv_bootstrap, 'bootstrap-ubuntu-12-04'
 set :ruby_version, '1.9.3-p429'
 set :rails_env, 'production'
 
 ask :db_password, "change_me"
-ask :db_ip, "111.111.111"
+ask :db_ip, "localhost"
+
+ask :stripe_private_key, "sk_test_key"
+ask :stripe_public_key, "pk_test_key"
+
+set :format, :pretty
+set :log_level, :debug
+# set :pty, true
+
+set :linked_files, %w{config/database.yml config/initializers/stripe.rb config/nginx.conf config/unicorn.rb config/unicorn_init.sh}
+set :linked_dirs, %w{bin} # log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 SSHKit.config.command_map[:rake]	= "bundle exec rake"
 SSHKit.config.command_map[:rails]	= "bundle exec rails"
@@ -32,14 +35,7 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'unicorn:restart'
-    end
-    
-		on roles(:web), in: :sequence, wait: 10 do
-			invoke 'nginx:restart'
-		end
+  	invoke 'nginx:restart'
+  	invoke 'unicorn:restart'
 	end
-
-  after :finishing, 'deploy:cleanup'
 end
